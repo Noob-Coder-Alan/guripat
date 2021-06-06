de6e830a3be9ec0fa7275ff591a60876005e3755
Alanray Baltazar Elangos <ealanray@gmail.com>
2021-06-06 15:16:41 +0800

    feat: added list provider

--- frontend/lib/providers/item_provider.dart
+++ frontend/lib/providers/item_provider.dart
@@ -3,5 +3,6 @@ import 'package:frontend/models/item.dart';
 
 class ItemProvider extends ChangeNotifier {
   List<Item> listItems = [];
+  
 }
 
--- frontend/lib/providers/list_provider.dart
+++ frontend/lib/providers/list_provider.dart
@@ -1,14 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/datasources/list/list_remote_datasource.dart';
 import 'package:frontend/models/ItemList.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 class ListProvider extends ChangeNotifier {
   ItemList list = ItemList(id: 0, code: "");
+  
+  ItemListRemoteDatasource datasource = ItemListRemoteDatasource(
+    client: GraphQLClient(
+      cache: GraphQLCache(),
+      link: HttpLink("http://localhost:5000/graphql")
+    )
+  );
 
-  void setAccessCode(String code) {
+  void setAccessCode(int id, String code) {
     list.code = code;
+    list.id = id;
+    notifyListeners();
   }
 
   void deleteAccessCode() {
     list.code = "";
+    list.id = 0;
+    notifyListeners();
+  }
+
+  Future<bool> checkConnection() async {
+    var isConnected = await datasource.checkConnection();
+    return isConnected;
+  }
+
+  Future<bool> isCodeValid(String code) async {
+    var isValid = await datasource.checkConnection();
+    return isValid;
+  }
+
+  Future<bool> deleteList(String code) async {
+    var isDeleted = await datasource.deleteList(code);
+    return isDeleted;
+  }
+
+  Future<ItemList> generateListCode() async {
+    var list = await datasource.generateListCode();
+    return list;
   }
 }
