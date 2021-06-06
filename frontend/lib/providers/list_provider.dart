ea9e6800363fbb2bd78972cf3b011bb433bb9cbf
Alanray Baltazar Elangos <ealanray@gmail.com>
2021-06-07 04:50:27 +0800

    feat: added sharedPreferences

--- frontend/lib/providers/list_provider.dart
+++ frontend/lib/providers/list_provider.dart
@@ -1,22 +1,36 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:frontend/datasources/list/list_remote_datasource.dart';
 import 'package:frontend/models/ItemList.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ListProvider extends ChangeNotifier {
   ItemList list = ItemList(id: 0, code: "");
   ItemListRemoteDatasource datasource;
+  Future<SharedPreferences> localStorageInstance;
 
-  ListProvider({required this.datasource});
+  ListProvider({required this.datasource, required this.localStorageInstance});
 
   void setAccessCode(int id, String code) async {
       list.code = code;
       list.id = id;
+
+      var localStorage = await localStorageInstance;
+      await localStorage.setString("accessCode", code);
+      
+      var jsonString = json.encode([]);
+      await localStorage.setString("items", jsonString);
+      
     notifyListeners();
   }
 
-  void deleteAccessCode() {
+  void deleteAccessCode() async {
     list.code = "";
     list.id = 0;
+
+    var localStorage = await SharedPreferences.getInstance();
+    localStorage.remove("accessCode");
     notifyListeners();
   }
 
